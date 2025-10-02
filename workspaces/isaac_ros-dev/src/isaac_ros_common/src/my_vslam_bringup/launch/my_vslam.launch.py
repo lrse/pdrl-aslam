96a8eaadd5445e4707d480ab10e5dfd070633842
WakkoW <arcelloberamartin@gmail.com>
2025-10-02 14:37:11 -0300

    cuVSLAM launch update.

--- workspaces/isaac_ros-dev/src/isaac_ros_common/src/my_vslam_bringup/launch/my_vslam.launch.py
+++ workspaces/isaac_ros-dev/src/isaac_ros_common/src/my_vslam_bringup/launch/my_vslam.launch.py
@@ -6,7 +6,6 @@ from launch_ros.descriptions import ComposableNode
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    # --- LaunchConfigs ---
     ns      = LaunchConfiguration('ns')
     base    = LaunchConfiguration('base_frame')
     ncam    = LaunchConfiguration('num_cameras')
@@ -21,8 +20,7 @@ def generate_launch_description():
     pub_tf_odom_base = LaunchConfiguration('publish_odom_to_base_tf')
     pub_tf_map_odom  = LaunchConfiguration('publish_map_to_odom_tf')
 
-    # --- Static TFs (publish on /tf_static before cuVSLAM starts) ---
-    # args: x y z yaw pitch roll parent child
+    # Static TFs publish.
     left_cam_static = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
@@ -44,7 +42,7 @@ def generate_launch_description():
         ],
     )
 
-    # --- cuVSLAM component ---
+    # cuVSLAM.
     vslam = ComposableNode(
         package='isaac_ros_visual_slam',
         plugin='nvidia::isaac_ros::visual_slam::VisualSlamNode',
@@ -63,8 +61,8 @@ def generate_launch_description():
             'enable_imu_fusion':          ParameterValue(imu_on,  value_type=bool),
             'rectified_images':           ParameterValue(rect,    value_type=bool),
             'sync_matching_threshold_ms': ParameterValue(sync_ms, value_type=float),
-            'image_qos':'SENSOR_DATA',  #I HAD 'DEFAULT' but changed it when I changed QOS Image and Camera (to BEST_EFFORT) info on the bridge. Now it has SENSOR_DATA
-            'image_buffer_size': 80, #JUST ADDED FOR MS POTENTIAL IMPROVEMENT. USED TO BE 80 BEFORE IMU
+            'image_qos':'SENSOR_DATA', 
+            'image_buffer_size': 80, 
 
 	    'enable_slam_visualization':  ParameterValue(
 		                                LaunchConfiguration('enable_slam_visualization'),
@@ -79,7 +77,6 @@ def generate_launch_description():
             
             
         }],
-        #extra_arguments=[{'use_intra_process_comms': True}],  #TRYING  THIS FOR REDUCING MS
     )
 
     container = ComposableNodeContainer(
@@ -93,25 +90,25 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('ns',                 default_value='unitree_go2'),
-        DeclareLaunchArgument('base_frame',         default_value='unitree_go2/base_footprint'), #CHANGED FROM base_link
+        DeclareLaunchArgument('base_frame',         default_value='unitree_go2/base_footprint'),
         DeclareLaunchArgument('num_cameras',        default_value='2'),
         DeclareLaunchArgument('min_num_images',     default_value='2'),
-        DeclareLaunchArgument('enable_imu_fusion',  default_value='false'),			#USED TO BE false but 9/9 changes
+        DeclareLaunchArgument('enable_imu_fusion',  default_value='false'),
         DeclareLaunchArgument('rectified_images',   default_value='true'),
-        DeclareLaunchArgument('sync_matching_threshold_ms', default_value='10.0'),    #USED TO BE 10.0 but trying to improve ms
+        DeclareLaunchArgument('sync_matching_threshold_ms', default_value='10.0'),
 
         DeclareLaunchArgument('use_sim_time', default_value='true'),
         DeclareLaunchArgument('map_frame',    default_value=[ns, TextSubstitution(text='/map')]),
         DeclareLaunchArgument('odom_frame',   default_value=[ns, TextSubstitution(text='/odom')]),
-        DeclareLaunchArgument('publish_odom_to_base_tf', default_value='true'),	 #USED TO BE false but 9/9 changes
-        DeclareLaunchArgument('publish_map_to_odom_tf',  default_value='true'),	#CHANGED FROM TRUE SINCE IM TRYING SLAM_TOOLBOX TO PUBLISH
+        DeclareLaunchArgument('publish_odom_to_base_tf', default_value='true'),
+        DeclareLaunchArgument('publish_map_to_odom_tf',  default_value='true'),
         
-        DeclareLaunchArgument('enable_slam_visualization',  default_value='false'),	#WAS TRUE BUT CHANGED TO DECREASE GPU USAGE
-        DeclareLaunchArgument('enable_observations_view',  default_value='false'),	#WAS TRUE BUT CHANGED TO DECREASE GPU USAGE
-        DeclareLaunchArgument('enable_landmarks_view',  default_value='false'),		#WAS TRUE BUT CHANGED TO DECREASE GPU USAGE
+        DeclareLaunchArgument('enable_slam_visualization',  default_value='false'),
+        DeclareLaunchArgument('enable_observations_view',  default_value='false'),
+        DeclareLaunchArgument('enable_landmarks_view',  default_value='false'),
 
 
-        # Start statics first, then cuVSLAM
+        # Start statics first, then cuVSLAM.
         left_cam_static,
         right_cam_static,
         container,
